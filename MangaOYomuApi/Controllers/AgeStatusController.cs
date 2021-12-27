b608e6affa0ae0b1b458d6c0279b88ffb1ee09ad
Waldemar_SSS <avvakumov112003@gmail.com>
2021-12-28 01:35:40 +0300

    Add AgeStatusController

---
+++ MangaOYomuApi/Controllers/AgeStatusController.cs
@@ -0,0 +1,30 @@
+﻿using MangaOYomu;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MangaOYomuApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AgeStatusController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<AgeStatus> Get()
+        {
+            return DataAccess.GetAgeStatuses();
+        }
+
+        [HttpGet("{AgeStatusID}")]
+        public ActionResult<AgeStatus> Get(int ageStatusID)
+        {
+            var result = DataAccess.GetAgeStatus(ageStatusID);
+            if (result == null)
+                return NotFound();
+
+            return result;
+        }
+    }
+}
