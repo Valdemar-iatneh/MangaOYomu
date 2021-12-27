e8509e7e20f098223e0bbb468ddb508e690f4352
Waldemar_SSS <avvakumov112003@gmail.com>
2021-12-28 01:31:07 +0300

    Add MangaTypeController and TitleStatusController

---
+++ MangaOYomuApi/Controllers/MangaTypeController.cs
@@ -0,0 +1,30 @@
+﻿using MangaOYomu;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MangaOYomuApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MangaTypeController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<MangaType> Get()
+        {
+            return DataAccess.GetMangaTypes();
+        }
+
+        [HttpGet("{MangaTypeID}")]
+        public ActionResult<MangaType> Get(int mangaTypeID)
+        {
+            var result = DataAccess.GetMangaType(mangaTypeID);
+            if (result == null)
+                return NotFound();
+
+            return result;
+        }
+    }
+}
---
+++ MangaOYomuApi/Controllers/TitleStatusController.cs
@@ -0,0 +1,30 @@
+﻿using MangaOYomu;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MangaOYomuApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TitleStatusController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<TitleStatus> Get()
+        {
+            return DataAccess.GetTitleStatuses();
+        }
+
+        [HttpGet("{TitleStatusID}")]
+        public ActionResult<TitleStatus> Get(int titleStatusID)
+        {
+            var result = DataAccess.GetTitleStatus(titleStatusID);
+            if (result == null)
+                return NotFound();
+
+            return result;
+        }
+    }
+}
