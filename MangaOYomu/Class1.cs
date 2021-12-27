5601fec385291b3449169e2dfa2d5fdbf335ba9a
Waldemar_SSS <avvakumov112003@gmail.com>
2021-12-27 20:13:22 +0300

    Add project files.

---
+++ MangaOYomu/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangaOYomu
+{
+    public class Class1
+    {
+    }
+}
