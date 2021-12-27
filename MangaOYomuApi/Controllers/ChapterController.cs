2019644380e46edad86b61a5e81c6ee734bc23ff
Waldemar_SSS <avvakumov112003@gmail.com>
2021-12-28 01:50:41 +0300

    Add ChapterController

---
+++ MangaOYomuApi/Controllers/ChapterController.cs
@@ -0,0 +1,37 @@
+﻿using MangaOYomu;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MangaOYomuApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChapterController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Chapters> Get()
+        {
+            return DataAccess.GetChapters();
+        }
+
+        [HttpGet("{ChaptersID}")]
+        public ActionResult<Chapters> Get(int chaptersID)
+        {
+            var result = DataAccess.GetChapter(chaptersID);
+            if (result == null)
+                return NotFound();
+
+            return result;
+        }
+
+        [HttpPost]
+        public IActionResult Create(Chapters chapter)
+        {
+            DataAccess.AddNewChapter(chapter);
+            return NoContent();
+        }
+    }
+}
