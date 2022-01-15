56bcc641991ad801c3238d0e9f3503ee6d8c7426
Waldemar_SSS <avvakumov112003@gmail.com>
2022-01-15 10:20:02 +0300

    Some

---
+++ MangaOYomuWeb/Controllers/MangaController.cs
@@ -0,0 +1,22 @@
+﻿using MangaOYomu;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MangaOYomuWeb.Controllers
+{
+    public class MangaController : Controller
+    {
+        //public async Task<IActionResult> Index(int id)
+        //{
+        //    return View(await DataAccess.GetMangaTitle(id);
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
